be5e02149cae31f1dd6e5f28a87428b6d9bec9ac
Yuri Demyrov <yuriy.demirov@difreight.com>
2026-01-12 16:09:25 +0200

    init

---
+++ micro-services/gateway/src/main.ts
@@ -0,0 +1,22 @@
+import { Logger } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { NestFactory } from '@nestjs/core'
+
+import { AppModule } from './app.module'
+
+async function bootstrap() {
+	const app = await NestFactory.create(AppModule)
+	const config = app.get(ConfigService)
+	const logger = new Logger()
+
+	app.enableCors({
+		origin: config.getOrThrow<string>('HTTP_CORS').split(','),
+		credentials: true
+	})
+	const port = config.getOrThrow<number>('HTTP_PORT')
+	const host = config.getOrThrow<string>('HTTP_HOST')
+	app.setGlobalPrefix('api')
+	await app.listen(port)
+	logger.log(`Gateway service is running : ${host}`)
+}
+bootstrap()
